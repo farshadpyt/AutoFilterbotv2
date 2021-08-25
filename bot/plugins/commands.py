be0a7229be27912b2b2bdbb9f23d7224234d6243
farshadpyt <89522593+farshadpyt@users.noreply.github.com>
2021-08-25 17:39:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name} /n @serieskodathi",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '👉Join group👈', url="https://t.me/serieskodathi"
                                 )
                         ]
                     ]
